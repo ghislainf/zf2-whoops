810c92f8b907cc0517c6dc4d3725ee6148cc24f0
ghislainf <ghislain.de.fontenay@gmail.com>
2013-06-08 22:35:16 +0200

    Refactoring and fix #2

--- Module.php
+++ Module.php
@@ -1,88 +1,90 @@
 <?php
 namespace Zf2Whoops;
 
-use Whoops\Run;
-use Whoops\Provider\Zend\ExceptionStrategy;
-use Whoops\Provider\Zend\RouteNotFoundStrategy;
-use Whoops\Handler\JsonResponseHandler;
-use Whoops\Handler\PrettyPageHandler;
 use Zend\EventManager\EventInterface;
 use Zend\Console\Request as ConsoleRequest;
+use Zend\Stdlib\ResponseInterface as Response;
+use Zend\Mvc\Application;
+use Zend\Mvc\MvcEvent;
+use Whoops\Run;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Handler\PrettyPageHandler;
 
 class Module
 {
-    protected $run;
+    /**
+     * @var Whoops\Run
+     */
+    protected $run = null;
 
-    public function onBootstrap(EventInterface $event)
+    /**
+     * {@inheritDoc}
+     */
+    public function onBootstrap(EventInterface $e)
     {
-        $prettyPageHandler = new PrettyPageHandler();
+        $config  = $e->getTarget()->getServiceManager()->get('Config');
+        $config  = isset($config['view_manager']) ? $config['view_manager'] : array();
 
-        // Set editor
-        $config = $event->getApplication()->getServiceManager()->get('Config');
-        if (isset($config['view_manager']['editor'])) {
-            $prettyPageHandler->setEditor($config['view_manager']['editor']);
+        if ($e->getRequest() instanceof ConsoleRequest || empty($config['display_exceptions'])) {
+            return;
         }
 
         $this->run = new Run();
         $this->run->register();
-        $this->run->pushHandler($prettyPageHandler);
 
-        $this->attachListeners($event);
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
-
-    private function attachListeners(EventInterface $event)
-    {
-        $request = $event->getRequest();
-        $application = $event->getApplication();
-        $services = $application->getServiceManager();
-        $events = $application->getEventManager();
-        $config = $services->get('Config');
-
-        //Display exceptions based on configuration and console mode
-        if ($request instanceof ConsoleRequest || empty($config['view_manager']['display_exceptions']))
-            return;
-
+        // set up whoops config
+        $prettyPageHandler = new PrettyPageHandler();
         $jsonHandler = new JsonResponseHandler();
 
-        if (!empty($config['view_manager']['json_exceptions']['show_trace'])) {
-            //Add trace to the JSON output
+        if (isset($config['editor'])) {
+            $prettyPageHandler->setEditor($config['editor']);
+        }
+
+        if (!empty($config['json_exceptions']['show_trace'])) {
             $jsonHandler->addTraceToOutput(true);
         }
 
-        if (!empty($config['view_manager']['json_exceptions']['ajax_only'])) {
-            //Only return JSON response for AJAX requests
+        if (!empty($config['json_exceptions']['ajax_only'])) {
             $jsonHandler->onlyForAjaxRequests(true);
         }
 
-        if (!empty($config['view_manager']['json_exceptions']['display'])) {
-            //Turn on JSON handler
+        if (!empty($config['json_exceptions']['display'])) {
             $this->run->pushHandler($jsonHandler);
         }
 
-        //Attach the Whoops ExceptionStrategy
-        $exceptionStrategy = new ExceptionStrategy($this->run);
-        $exceptionStrategy->attach($events);
+        $this->run->pushHandler($prettyPageHandler);
 
-        //Attach the Whoops RouteNotFoundStrategy
-        $routeNotFoundStrategy = new RouteNotFoundStrategy($this->run);
-        $routeNotFoundStrategy->attach($events);
-
-        //Detach default ExceptionStrategy
-        $services->get('Zend\Mvc\View\Http\ExceptionStrategy')->detach($events);
-
-        //Detach default RouteNotFoundStrategy
-        $services->get('Zend\Mvc\View\Http\RouteNotFoundStrategy')->detach($events);
+        $eventManager = $e->getTarget()->getEventManager();
+        $eventManager->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'prepareException'));
+        $eventManager->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'prepareException'));
     }
 
+    /**
+     * Whoops handle exceptions
+     * @param Zend\Mvc\MvcEvent $e
+     */
+    public function prepareException(MvcEvent $e)
+    {
+        $error = $e->getError();
+        if (!empty($error) && !$e->getResult() instanceof Response) {
+            switch ($error) {
+                case Application::ERROR_CONTROLLER_NOT_FOUND:
+                case Application::ERROR_CONTROLLER_INVALID:
+                case Application::ERROR_ROUTER_NO_MATCH:
+                    // Specifically not handling these
+                    return;
+
+                case Application::ERROR_EXCEPTION:
+                default:
+                    $response = $e->getResponse();
+                    if (!$response || $response->getStatusCode() === 200) {
+                        header('HTTP/1.0 500 Internal Server Error', true, 500);
+                    }
+
+                    ob_clean();
+                    $this->run->handleException($e->getParam('exception'));
+                    break;
+            }
+        }
+    }
 }
--- config/zf2-whoops.local.php
+++ config/zf2-whoops.local.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'view_manager' => array(
-    	'editor' => 'sublime',
+        'editor' => 'sublime',
         'display_not_found_reason' => true,
         'display_exceptions' => true,
         'json_exceptions' => array(
